4172c785ad92d92d70156f703dd75e89d5b8fbcd
BYJ <kmsjhj07@gmail.com>
2025-05-01 12:07:37 +0900

    홈서비스 스타일 구현

--- app/routes/pages/home/components/home-service.tsx
+++ app/routes/pages/home/components/home-service.tsx
@@ -1,28 +1,55 @@
 export default function HomeService() {
   return (
-    <section className="h-[1687px] bg-amber-100">
-      <div className="container flex h-full bg-white">
-        <div className="flex-1 bg-blue-300">
-          <div>1-1</div>
+    <section className="h-[1687px]">
+      <div className="container flex h-full px-[46px] pt-[250px] pb-[165px]">
+        <div className="flex-1">
+          <div>
+            <h2 className="mb-[30px] text-[28px] leading-[1.3] font-[700] text-[#3182f6]">
+              홈 · 소비
+            </h2>
+            <p className="text-[50px] leading-[1.4] font-[700] text-[#191f28]">
+              내 돈 관리,
+              <br />
+              지출부터 일정까지
+              <br />
+              똑똑하게
+            </p>
+          </div>
           <div className="relative">
             <img
-              className="absolute top-[83px] left-[110px] w-[303px]"
+              className="absolute top-[75px] left-[32px] w-[275px]"
               src="/images/home-service-1.png"
               alt="home-service-1"
             />
-            <img className="relative w-[600px]" src="/images/iphone.png" alt="iphone" />
+            <img
+              className="relative left-[-70px] w-[600px]"
+              src="/images/iphone.png"
+              alt="iphone"
+            />
           </div>
         </div>
-        <div className="flex-1 bg-red-300">
+        <div className="flex flex-1 flex-col items-end">
           <div className="relative">
             <img
-              className="absolute top-[83px] left-[110px] w-[303px]"
+              className="absolute top-[75px] left-[92px] w-[275px]"
               src="/images/home-service-2.png"
               alt="home-service-2"
             />
-            <img className="relative w-[600px]" src="/images/iphone.png" alt="iphone" />
+            <img
+              className="relative left-[-10px] w-[600px]"
+              src="/images/iphone.png"
+              alt="iphone"
+            />
           </div>
-          <div>2-2</div>
+          <div>
+            <p className="text-[rgb(51, 61, 75)] pt-[30px] text-[23px] leading-[1.5] font-[600]">
+              토스에 계좌와 카드를 연결해 보세요
+              <br />
+              계좌 잔액, 대출·투자 내역은 기본,
+              <br />
+              일자별 소비와 수입까지 한 번에 볼 수 있어요.
+            </p>
+          </div>
         </div>
       </div>
     </section>
