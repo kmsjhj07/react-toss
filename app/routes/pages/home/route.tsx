a32262f35ebd25f86afea297bb2b7b164bafb495
BYJ <kmsjhj07@gmail.com>
2025-05-01 10:50:18 +0900

    설명문 섹션 추가

--- app/routes/pages/home/route.tsx
+++ app/routes/pages/home/route.tsx
@@ -1,5 +1,11 @@
+import Description from './components/description';
 import Hero from './components/hero';
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Description />
+    </>
+  );
 }
