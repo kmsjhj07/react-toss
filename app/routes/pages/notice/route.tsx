6ad8af748a8f06f75bb19517c638c9f7d2e8466c
BYJ <kmsjhj07@gmail.com>
2025-05-12 11:12:40 +0900

    공지사항 리스트 스타일 적용

--- app/routes/pages/notice/route.tsx
+++ app/routes/pages/notice/route.tsx
@@ -1,3 +1,55 @@
+import NoticeItem from './components/notice-item';
+import NoticePagination from './components/notice-pagination';
+
+interface Notice {
+  title: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const notices: Notice[] = [
+  {
+    title: '공지사항 예시 1',
+    content: '공지사항 내용 1',
+    createdAt: new Date('2025-05-10'),
+    updatedAt: new Date('2025-05-10'),
+  },
+  {
+    title: '공지사항 예시 2',
+    content: '공지사항 내용 2',
+    createdAt: new Date('2025-05-11'),
+    updatedAt: new Date('2025-05-11'),
+  },
+  {
+    title: '공지사항 예시 3',
+    content: '공지사항 내용 3',
+    createdAt: new Date('2025-05-12'),
+    updatedAt: new Date('2025-05-12'),
+  },
+];
+
 export default function Notice() {
-  return <>notice</>;
+  return (
+    <section>
+      <div className="container">
+        <h1 className="pt-[168px] pb-[25px] text-[44px] leading-[66px] font-[700]">
+          공지사항
+        </h1>
+        <div>
+          {notices.map((notice, index) => (
+            <NoticeItem
+              key={index}
+              title={notice.title}
+              createdAt={notice.createdAt}
+              isLast={index === notices.length - 1}
+            />
+          ))}
+        </div>
+        <div className="pt-[50px]">
+          <NoticePagination />
+        </div>
+      </div>
+    </section>
+  );
 }
