141a8a4875eff4692a98cb3bcec7b3e877359822
BYJ <kmsjhj07@gmail.com>
2025-05-14 09:59:12 +0900

    vite 설정에 viteStaticCopy 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -13,6 +14,14 @@ export default defineConfig({
       svgrOptions: {
         // svgr options
       },
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './app/generated/prisma/**/*.node',
+          dest: './',
+        },
+      ],
     }),
   ],
 });
